4a58dce246e2ef8c2c8557605a1c951e043e6dff
jasonZ <1747291545@qq.com>
2022-11-24 21:34:17 +0800

    雏形

--- src/main/java/com/hunau/template/listener/NettyStartListener.java
+++ src/main/java/com/hunau/template/listener/NettyStartListener.java
@@ -3,6 +3,7 @@ package com.hunau.template.listener;
 import com.hunau.template.netty.server.SocketServer;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
@@ -10,6 +11,7 @@ import javax.annotation.Resource;
  * 监听spring启动后
  * 启动netty
  * */
+@Component
 public class NettyStartListener implements ApplicationRunner {
     @Resource
     private SocketServer server;
